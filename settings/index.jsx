48671e37d5a86fa0742c8a6d0c83cdfa8597b09d
Chris Spooner <cmspooner@gmail.com>
2018-03-24 12:59:13 -0400

    Debugging Options and Update Log added

--- settings/index.jsx
+++ settings/index.jsx
@@ -12,15 +12,23 @@ function mySettings(props) {
             {name:"1 hour"},
             {name:"2 hours"},
           ]}
-          />
-         <Toggle
-          settingsKey="unitToggle"
-          label="Tempterature in °F or °C"
          />
          <Toggle
-          settingsKey="dataAgeToggle"
-          label="Show age of last weather update"
-        />
+           settingsKey="unitToggle"
+           label="Tempterature in °F or °C"
+         />
+         <Toggle
+           settingsKey="dataAgeToggle"
+           label="Show time of last weather update"
+         />
+         <Toggle
+           settingsKey="errorMessageToggle"
+           label="Show weather error messages"
+         />
+         <Toggle
+           settingsKey="failCountToggle"
+           label="Show number of weather attempts"
+         />
       </Section>
       <Section
         title={<Text bold align="center">Seperator Bar Color</Text>}>
@@ -56,6 +64,48 @@ function mySettings(props) {
             {name:"Exam"},
           ]}
           />
+      </Section>
+      <Section
+        title={<Text bold align="center">Build Version and Notes</Text>}>
+        <Text>
+          Beta 2.1.8: Weather should update in background, all intervals now in x*1000 notation.
+        </Text>
+        <Text>
+          Beta 2.1.7: Made changes to weather error handling.
+        </Text>
+        <Text>
+          Beta 2.1.6: Fixed Rain showing as thunderstorm and added comments to weather icon switch statement
+        </Text>
+        <Text>
+          Beta 2.1.5: Added this section
+        </Text>
+        <Text>
+          Beta 2.1.4.2: Added Error Message to Weather
+        </Text>
+        <Text>
+          Beta 2.1.4.1: Fixed updating message
+        </Text>
+        <Text>
+          Beta 2.1.4: Updating Message Added
+        </Text>
+        <Text>
+          Beta 2.1.2.1: Reverted Battery Change
+        </Text>
+        <Text>
+          Beta 2.1.3: Chaged Battery
+        </Text>
+        <Text>
+          Beta 2.1.2: Clean Up Battery
+        </Text>
+        <Text>
+          Beta 2.1.1: Added Battery Percentage Color based on level
+        </Text>
+        <Text>
+          Beta 2.1: Added Battery Percentage.
+        </Text>
+        <Text>
+          Beta 2.0: Added Weather.
+        </Text>
       </Section>
     </Page>
   );
