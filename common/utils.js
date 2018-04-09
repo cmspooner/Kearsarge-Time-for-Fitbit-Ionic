2b7b9b156895af97129b2393e2fcbfa4e961b333
Chris Spooner <cmspooner@gmail.com>
2018-04-09 12:00:38 -0400

    Moved icon code into utils

--- common/utils.js
+++ common/utils.js
@@ -125,3 +125,46 @@ export function wordStartsWith(letter, text){
   }
     return false;
 }
+
+export function getWeatherIcon(data){
+  switch(data.conditionCode){
+    case 0: //ClearSky
+      if (data.isDay)
+        return "../resources/icons/weather/whiteSun.png"
+      else
+        return "../resources/icons/weather/whiteMoon.png" 
+      break;
+    case 1: //FewClouds
+    case 2: //ScatteredClouds
+      if (data.isDay)
+        return "../resources/icons/weather/whitePartlySunny.png"
+      else
+        return "../resources/icons/weather/whitePartlyMoon.png"
+      break;
+    case 3: //BrokenClouds
+      return "../resources/icons/weather/whiteCloud.png"
+      break;
+    case 4: //ShowerRain
+    case 5: //Rain
+     return "../resources/icons/weather/whiteRain.png"
+      break;
+    case 6: //Thunderstorm
+      if (wordStartsWith("T", data.description))
+        return "../resources/icons/weather/whiteStorm.png"
+      else
+        return "../resources/icons/weather/whiteRain.png"
+      break;
+    case 7: //Snow
+      return "../resources/icons/weather/whiteSnow.png"
+      break;
+    case 8: //Mist
+      weatherImage.href = "../resources/icons/weather/whiteHaze .png"
+      break;
+    default: //Other
+      if (data.isDay)
+        return "../resources/icons/weather/whiteSun.png"
+      else
+        return "../resources/icons/weather/whiteMoon.png"
+      break;
+  }
+}
